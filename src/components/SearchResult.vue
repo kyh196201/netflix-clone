92c907d6d6aabd90f987443b36850cba55cecf8a
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2020-11-12 01:30:32 +0900

    Search 컴포넌트에서 SearchResult, NoResult 컴포넌트 관리하도록 변경

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,68 +1,38 @@
 <template>
     <div class="searchResult">
-        <template v-if="movieList && movieList.length">
-            <ul class="movie-list">
-                <li
-                    class="movie-list__item"
-                    v-for="movie in movieList"
-                    :key="movie.id"
-                    :data-movie-id="movie.id"
-                >
-                    <div class="movie">
-                        <figure class="movie__thumbnail">
-                            <img
-                                :src="getPoster(movie['poster_path'])"
-                                :alt="movie.title"
-                                @error="onError"
-                            />
-                        </figure>
-                    </div>
-                </li>
-            </ul>
-        </template>
-        <no-result v-else :keyword="inputKeyword" />
+        <ul class="movie-list">
+            <li
+                class="movie-list__item"
+                v-for="movie in data"
+                :key="movie.id"
+                :data-movie-id="movie.id"
+            >
+                <div class="movie">
+                    <figure class="movie__thumbnail">
+                        <img
+                            :src="getPoster(movie['poster_path'])"
+                            :alt="movie.title"
+                            @error="onError"
+                        />
+                    </figure>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
 import { IMG_PATH } from "../utils/constant.js";
 import defaultImage from "../assets/images/default_image.png";
-import NoResult from "./NoResult.vue";
 
 export default {
     name: "SearchResult",
-    components: {
-        "no-result": NoResult,
-    },
+    props: ["data"],
     data() {
         return {
             imgPath: IMG_PATH,
             defaultImage: defaultImage,
-            inputKeyword: "",
         };
-    },
-    computed: {
-        ...mapState({
-            list: (state) => state.searchResult,
-            keyword: (state) => state.searchKeyword,
-        }),
-        page() {
-            return this.list.page;
-        },
-        totalPage() {
-            return this.list["total_pages"];
-        },
-        movieList() {
-            return this.list.results;
-        },
-        isMoreData() {
-            return this.totalPage > this.page;
-        },
-    },
-    created() {
-        // FIXME this.keyword값은 searchInput의 input태그와 양방향 바인딩되어있으므로, input의 값에 따라 변하지 않도록 새로운 데이터를 추가
-        this.inputKeyword = this.keyword;
     },
     methods: {
         getPoster(poster) {
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,13 +1,15 @@
 <template>
     <section class="search">
         <search-history />
-        <search-result />
+        <search-result v-if="movieList && movieList.length" :data="movieList" />
+        <no-result v-else :keyword="inputKeyword" />
     </section>
 </template>
 
 <script>
 import SearchResult from "../components/SearchResult.vue";
 import SearchHistory from "../components/SearchHistory.vue";
+import NoResult from "../components/NoResult.vue";
 import { mapState, mapMutations } from "vuex";
 
 export default {
@@ -15,12 +17,31 @@ export default {
     components: {
         "search-result": SearchResult,
         "search-history": SearchHistory,
+        "no-result": NoResult,
+    },
+    data() {
+        return {
+            inputKeyword: "",
+        };
     },
     computed: {
         ...mapState({
             isSearching: (state) => state.isSearching,
             keyword: (state) => state.searchKeyword,
+            list: (state) => state.searchResult,
         }),
+        page() {
+            return this.list.page;
+        },
+        totalPage() {
+            return this.list["total_pages"];
+        },
+        movieList() {
+            return this.list.results;
+        },
+        isMoreData() {
+            return this.totalPage > this.page;
+        },
     },
     created() {
         if (!this.keyword) {
@@ -28,6 +49,7 @@ export default {
         }
 
         this.SET_IS_SEARCHING(true);
+        this.inputKeyword = this.keyword;
     },
     methods: {
         ...mapMutations(["SET_IS_SEARCHING"]),
